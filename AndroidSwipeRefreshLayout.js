387663624cdd1f5364772166a22bb4ca78f08583
li_zy <zhuoyuan93@gmail.com>
2017-08-18 14:47:17 +0800

    Update AndroidSwipeRefreshLayout.js

--- AndroidSwipeRefreshLayout.js
+++ AndroidSwipeRefreshLayout.js
@@ -1,47 +1,48 @@
+/**
+ * Created by jiajiewang on 2017/6/27.
+ */
 
-import React, {
-  PropTypes,
-  Component,
-} from 'react'
+import React from 'react';
 import {
-  View,
-  requireNativeComponent,
-  Platform,
-} from 'react-native'
+    View,
+    requireNativeComponent,
+    Platform,
+} from 'react-native';
 
-export default class AndroidSwipeRefreshLayout extends Component {
+export default class AndroidSwipeRefreshLayout extends React.Component {
 
-  static propTypes = {
-    ...View.propTypes,
-    refreshing: PropTypes.bool,
-    enabledPullUp: PropTypes.bool,
-    enabledPullDown: PropTypes.bool,
-    onSwipe: PropTypes.func,
-    onRefresh: PropTypes.func,
-  }
+    //类型判断
+    static propTypes = {
+        ...View.propTypes,
+        refreshing: React.PropTypes.bool,
+        enabledPullUp: React.PropTypes.bool,
+        enabledPullDown: React.PropTypes.bool,
+        onSwipe: React.PropTypes.func,
+        onRefresh: React.PropTypes.func,
+    }
 
-  setNativeProps(props) {
-    this._nativeSwipeRefreshLayout.setNativeProps(props)
-  }
+    setNativeProps(props) {
+        this._nativeSwipeRefreshLayout.setNativeProps(props)
+    }
 
-  _onSwipe = (e) => {
-    this.props.onSwipe(e.nativeEvent.movement)
-  }
+    _onSwipe = (e) => {
+        this.props.onSwipe(e.nativeEvent.movement)
+    }
 
-  _onRefresh = () => {
-    this.props.onRefresh()
-  }
+    _onRefresh = () => {
+        this.props.onRefresh()
+    }
 
-  render() {
-    return (
-      <NativeSwipeRefreshLayout
-          {...this.props}
-          ref={ (component) => this._nativeSwipeRefreshLayout = component }
-          onSwipe={this._onSwipe}
-          onSwipeRefresh={this._onRefresh}
-      />
-    );
-  }
+    render() {
+        return (
+            <NativeSwipeRefreshLayout
+                {...this.props}
+                ref={ (component) => this._nativeSwipeRefreshLayout = component }
+                onSwipe={this._onSwipe}
+                onSwipeRefresh={this._onRefresh}
+            />
+        );
+    }
 }
 
 const NativeSwipeRefreshLayout = Platform.OS == 'ios' ? View : requireNativeComponent('RCTSwipeRefreshLayout', AndroidSwipeRefreshLayout)
